d7acc3e58b0b7e502d31762e955ce5e50e43db22
Connor <connor.aitken@gmail.com>
2017-11-17 01:25:59 -0800

    re-enabling elecron

--- app/start.sh
+++ app/start.sh
@@ -11,4 +11,4 @@ cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
 # the syntax below starts an X istance with ONLY our electronJS fired up,
 # it saves you a LOT of resources avoiding full-desktops envs
 rm /tmp/.X0-lock &>/dev/null || true
-#startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
+startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
